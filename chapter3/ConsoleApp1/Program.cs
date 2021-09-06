18d8e4890f56a3d765f948fd38874cca78d21161
herbatataszatana <anma.swiatkowska@gmail.com>
2021-09-06 21:02:53 +0100

    Added comment

---
+++ chapter3/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+ Chapter 3 activites 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInteger;
+            string myString;
+            myInteger = 17;
+            myString = "\"myInteger\" is";
+            Console.WriteLine($"{myString} {myInteger}");
+            Console.ReadKey();
+        }
+    }
+}
